3fa12ee2d200d0706191574104f1a8f8b5f908c3
Andrew Hess <andrew7hess@gmail.com>
2020-05-17 18:36:27 -0600

    Define more expression types

--- exprs.py
+++ exprs.py
@@ -6,14 +6,29 @@ from type import Type
 
 
 class ExprEnum(Enum):
-    CREATE_VAR = 1
-    SET_VAR    = 2
-    DEFINE     = 3
-    CALL       = 4
+    LITERAL    = 1
+    CREATE_VAR = 2
+    SET_VAR    = 3
+    DEFINE     = 4
+    CALL       = 5
 
 
 class Expr:
     exprClass = None
+
+
+class Literal(Expr):
+    def __init__(self, _loc, _type, _val):
+        # Check that the type is as expected.
+        actual_type = Type.get_type(_val)
+        if (_type != actual_type):
+            raise TypeError(_loc, _type, actual_type)
+
+        # Setup the data.
+        self.exprClass = ExprEnum.LITERAL
+        self.loc = _loc     # Type Location
+        self.type = _type   # Type Type
+        self.val = _val     # Type corresponding to _type
 
 
 class CreateVar(Expr):
@@ -25,6 +40,33 @@ class CreateVar(Expr):
 
         # Setup the data.
         self.exprClass = ExprEnum.CREATE_VAR
+        self.loc = _loc     # Type Location
         self.type = _type   # Type Type
         self.name = _name   # Type string
-        self.val = _val     # Variable with type corresponding to _type
+        self.val = _val     # Type corresponding to _type
+
+
+class SetVar(Expr):
+    def __init__(self, _loc, _name, _val):
+        self.exprClass = ExprEnum.SET_VAR
+        self.loc = _loc     # Type Location
+        self.name = _name   # Type string
+        self.val = _val
+
+
+class Define(Expr):
+    def __init__(self, _loc, _return_type, _name, _args, _body):
+        self.exprClass = ExprEnum.DEFINE
+        self.loc = _loc                  # Type Location
+        self.return_type = _return_type  # Type Type
+        self.name = _name                # Type string
+        self.args = _args                # Type list of (Type, string) tuples
+        self.body = _body                # Type list of Expr's
+
+
+class Call(Expr):
+    def __init__(self, _loc, _name, _params):
+        self.exprClass = ExprEnum.CALL
+        self.loc = _loc         # Type Location
+        self.name = _name       # Type string
+        self.params = _params   # Type list of Expr's
