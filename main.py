42b0ea5ce3616b660412e0d72da8470b4fe9ee3e
Andrew Hess <andrew7hess@gmail.com>
2020-06-06 11:08:13 -0600

    Make and run executable from main.py
    Previously, main.py only generated the C++/CUDA code and to actually
    run it, you had to run `make' to create an executable and then manually
    invoke it. This has all been put into main.py for convenience.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from generator import Generator
 
+import os.path
+import subprocess
 import sys
 
 
@@ -17,7 +19,31 @@ def main():
     # and CUDA. The second command line arguments should be a boolean: True to
     # parallelize the code, and false to just convert it to C++.
     g = Generator(sys.argv[1])
-    g.generate(int(sys.argv[2]))
+    parallelized = g.generate(int(sys.argv[2]))
+
+    # The generator outputs cpp, cuda, and Makefile files, but does not run
+    # `make', so do that now.
+    (dir_path, file) = os.path.split(sys.argv[1])
+    process = subprocess.Popen('make', cwd=dir_path, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    (output, error) = process.communicate()
+
+    # The `make' program only has a non-zero return code if an error occured.
+    # Warnings do not yield a non-zero return code.
+    if process.returncode != 0:
+        # Print the errors for the user.
+        print(f'{error.decode("utf-8")}')
+        return process.returncode
+
+    # Run the executable created by `make'.
+    dir_path = '.' if dir_path == '' else dir_path
+    executable = file[:file.rfind('.')]
+    process = subprocess.Popen(f'{dir_path}/{executable}',
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    (output, error) = process.communicate()
+    print(f'{output.decode("utf-8")}', end='')
+    print(f'{error.decode("utf-8")}', end='')
 
 
 if __name__ == '__main__':
